31bcba5ce838657b1722e9c4dd592c4a68898b2a
Sebastian Barrionuebo <seb.barrionuebo@gmail.com>
2021-10-02 06:20:18 -0300

    Clase 6 - Quinta subida

--- src/Componets/ItemListContainer.js
+++ src/Componets/ItemListContainer.js
@@ -2,18 +2,17 @@ import ItemList from "./ItemList"
 import { useState, useEffect } from "react"
 
 const ItemListContainer = () => {
-    //Este array simula ser los datos de una API
-    const datosDeLaAPI  =   [{id: 1, name:"Producto 1"},
-                            {id: 1, name:"Producto 2"},
-                            {id: 1, name:"Producto 3"},
-                            {id: 1, name:"Producto 4"}]
-
     const [datosAPI, setdatosAPI] = useState(null)
     
     useEffect(() => {
         const pidiendoDatosALaAPI = new Promise((res,rej)=>{
             setTimeout(() => {
                 console.log("Pidiendo datos a la API")
+                //Este array simula ser los datos de una API
+                const datosDeLaAPI  =   [{id: 1, name:"Producto 1"},
+                            {id: 1, name:"Producto 2"},
+                            {id: 1, name:"Producto 3"},
+                            {id: 1, name:"Producto 4"}]
                 if(datosDeLaAPI != null){
                     res(datosDeLaAPI)
                 }else{
